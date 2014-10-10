ef9ad7d51d246918721a14f7bcace9d8bcb06ef4
harlanentler <harlanentler@gmail.com>
2014-10-10 01:53:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,18 +9,33 @@ typedef void (*fptr)(ecoNode x);
 
 void ecoNodePrint(ecoNode x);
 
+void ecoNodeAct(ecoNode x);
+
 int main()
 {
+    cout << "phase 1:" << endl << endl << endl << endl;
+
     theGrid<ecoNode, fptr> test(2,2);
-    fptr U=ecoNodePrint;
+    fptr a=ecoNodePrint;
+    fptr b=ecoNodeAct;
 
-    test.call(U);
+    test.call(a);
 
-    cout << "Hello World!" << endl;
+    cout << endl << endl << endl << endl << "phase 2:" << endl << endl << endl << endl;
+
+    test.call(b);
+
+    cout << endl << endl << endl << endl << "end" << endl << endl << endl << endl;
+
     return 0;
 }
 
 void ecoNodePrint(ecoNode x)
 {
     x.printMe();
+}
+
+void ecoNodeAct(ecoNode x)
+{
+    x.act();
 }
