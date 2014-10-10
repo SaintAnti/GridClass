22e36c2b5b1fbb8bb7c7b4451bb84cab2ba2a0da
harlanentler <harlanentler@gmail.com>
2014-10-10 01:54:45 -0700

    Update econode.cpp

--- econode.cpp
+++ econode.cpp
@@ -3,19 +3,29 @@
 ecoNode::ecoNode()
 {
     cout << "constructing ecoNode" << endl;
+    myEntity = new entity;
 }
 
 ecoNode::~ecoNode()
 {
     cout << "destructing ecoNode" << endl;
+    delete myEntity;
 }
 
 ecoNode::ecoNode(const ecoNode& other)
 {
     cout << "copy constructing ecoNode" << endl;
+    if(&other != this)
+        myEntity = new entity(*(other.myEntity));
 }
 
 void ecoNode::printMe()
 {
     cout << "I am being printed!" << endl;
+}
+
+void ecoNode::act()
+{
+    cout << "ecoNode act" << endl;
+    myEntity->act();
 }
